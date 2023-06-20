353932e333468a9dbd57887133b4948e5413f22e
damino312 <daminionq@gmail.com>
2023-06-20 22:08:48 +0700

    work on myrequsts

--- src/controllers/Booking.controller.js
+++ src/controllers/Booking.controller.js
@@ -5,55 +5,100 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = "sdafadfgjiaFDJA/;dfAJNKD;Nask"; // moveto .env
 
 const bookThePlace = async (req, res) => {
-  const { user, place, guests, checkIn, checkOut, firstName, phone } = req.body;
-  console.log(req.body);
+  const { token } = req.cookies;
+  const { user, owner, place, guests, checkIn, checkOut, firstName, phone } =
+    req.body;
 
-  const booking = new Booking({
-    place,
-    user,
-    checkIn,
-    checkOut,
-    name: firstName,
-    phone,
-    guests,
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    if (place.user == userData.id) {
+      res.json("owner");
+    } else {
+      const booking = new Booking({
+        place,
+        user,
+        checkIn,
+        checkOut,
+        name: firstName,
+        phone,
+        guests,
+        owner,
+      });
+      try {
+        await booking.save();
+        res.json("ok");
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to create booking" });
+      }
+    }
   });
-  try {
-    await booking.save();
-    res.json("ok");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to create booking" });
-  }
 };
 
 const getBookingsOfUser = async (req, res) => {
   const { token } = req.cookies;
-  try {
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-      if (err) throw err;
-      const { id: user } = userData;
-      res.json(await Booking.find({ user }).populate("place"));
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const { id: user } = userData;
+    res.json(await Booking.find({ user }).populate("place"));
+  });
 };
 
 const deleteBooking = async (req, res) => {
   const { id } = req.params;
   const { token } = req.cookies;
-  try {
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-      if (err) throw err;
-      const { user } = await Booking.findById(id);
-      if (user.toString() === userData.id) {
-        await Booking.findByIdAndDelete(id);
-        res.json("The booking has been deleted");
-      }
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const { user } = await Booking.findById(id);
+    if (user.toString() === userData.id) {
+      await Booking.findByIdAndDelete(id);
+      res.json("The booking has been deleted");
+    }
+  });
 };
 
-module.exports = { bookThePlace, getBookingsOfUser, deleteBooking };
+const getRequestsToMe = async (req, res) => {
+  const { token } = req.cookies;
+
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    res.json(
+      await Booking.find({ owner: userData.id, status: 0 }).populate("place")
+    );
+  });
+};
+
+const getHistoryRequestsToMe = async (req, res) => {
+  const { token } = req.cookies;
+
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    res.json(
+      await Booking.find({
+        owner: userData.id,
+        status: { $in: [1, 2] },
+      }).populate("place")
+    );
+  });
+};
+
+const changeStatusOfRequest = async (req, res) => {
+  const { token } = req.cookies;
+  const { status, ownerId, bookingId } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    if (userData.id === ownerId) {
+      await Booking.findByIdAndUpdate(bookingId, { status: status });
+      res.json("The status has been changed");
+    }
+  });
+};
+
+module.exports = {
+  bookThePlace,
+  getBookingsOfUser,
+  deleteBooking,
+  getRequestsToMe,
+  changeStatusOfRequest,
+  getHistoryRequestsToMe,
+};
--- src/routes/Booking.route.js
+++ src/routes/Booking.route.js
@@ -16,4 +16,10 @@ app.get("/bookings", BookingController.getBookingsOfUser);
 
 app.delete("/booking/:id", BookingController.deleteBooking);
 
+app.get("/myrequests", BookingController.getRequestsToMe);
+
+app.get("/myrequests/history", BookingController.getHistoryRequestsToMe);
+
+app.put("/myrequest", BookingController.changeStatusOfRequest);
+
 module.exports = app;
