4cdb765a5d338799b50e7afe91c5728114b96c06
damino312 <daminionq@gmail.com>
2023-06-20 22:09:09 +0700

    add more fields in the model

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -6,8 +6,10 @@ const BookingSchema = new Schema({
   checkIn: { type: Date, required: true },
   checkOut: { type: Date, required: true },
   name: { type: String, required: true },
+  owner: { type: String, required: true },
   phone: { type: String, required: true },
   guests: Number,
+  status: { type: Number, required: true, default: 0 },
 });
 
 const BookingModel = mongoose.model("Booking", BookingSchema);
